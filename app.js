6fed0165b0064921481adbe06a81add5c15dd76a
nadav5199 <nadavpassal@gmail.com>
2024-06-30 17:39:17 +0300

    main page

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const Movie = require('./DataBase/models/Movie');
+const mongoose = require('mongoose');
+const app = express();
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/movies');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+
+app.get('/store', async (req, res) => {
+    try {
+        const movies = await Movie.find({});
+        res.render('store', {movies});
+    } catch (err) {
+        console.error('Error fetching movies:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+app.get('/', (req, res) => {
+    res.send('Welcome to the fullstack app!');
+})
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+});

57116e273792e60770fe58fdb2b60ae22c157b5d
nadav5199 <nadavpassal@gmail.com>
2024-07-01 16:45:30 +0300

    added log out

--- app.js
+++ app.js
@@ -86,6 +86,15 @@ app.post('/signup', async (req, res) => {
     }
 });
 
+app.post('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.redirect('/');
+        }
+        res.clearCookie('connect.sid');
+        res.redirect('/signin');
+    });
+});
 
 app.get('/', async (req, res) => {
     const { sort, search } = req.query;
